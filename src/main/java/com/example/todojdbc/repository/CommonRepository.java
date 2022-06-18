e385db53354875a8a2e87a0a5202d31404e7afff
WoldemarK <kovtynov.vladimir@gmail.com>
2022-06-18 22:51:23 +0300

    Initial commit

---
+++ src/main/java/com/example/todojdbc/repository/CommonRepository.java
@@ -0,0 +1,19 @@
+package com.example.todojdbc.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface CommonRepository<T> {
+
+    public T save(T domain);
+
+    public Iterable<T> save(Collection<T> domain);
+
+    public void delete(T domain);
+
+    public T findById(String id);
+
+    public Iterable<T> findAll();
+}
